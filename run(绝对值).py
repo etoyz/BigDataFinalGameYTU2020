e3fded980342d9a3f853166cf73bf6f3a98ea4c8
eto-ytm <2905226519@qq.com>
2020-12-07 19:50:43 +0800

    v5

--- run(绝对值).py
+++ run(绝对值).py
@@ -13,39 +13,57 @@ to_predict = data2['to_predict']
 # k_mok = 180
 # k_umok = (200, 240)
 # 单独限制
-k_uok = 40
-k_mok = 40
+k_uok = 100000
+k_mok = 100000
 # 多重限制
-k_umok = (0, 0)
+k_umok = (15, 0)
 
 result = []
+jd = 0
 for it in to_predict:
     uid = it[0]
+    mid = it[1]
     u_rest = uid_only[uid]
     m_rest = mid_only[uid]
     u_res = []
     m_res = []
+    u_res2 = []
+    m_res2 = []
+    # 单独uid方式
     for u_re in u_rest.items():
         if u_re[1] > k_uok:
             u_res.append(u_re[0])
         else:
             break
+    if mid in u_res:
+        result.append(1)
+        continue
+
+    # 单独mid方式
     for m_re in m_rest.items():
         if m_re[1] > k_mok:
             m_res.append(m_re[0])
         else:
             break
-
-    mid = it[1]
-    if mid in u_res:
-        result.append(1)
-        continue
     if mid in m_res:
         result.append(1)
         continue
-    if mid in u_res and mid in m_res:
+
+    # 混合方式
+    for u_re in u_rest.items():
+        if u_re[1] > k_umok[0]:
+            u_res2.append(u_re[0])
+        else:
+            break
+    for m_re in m_rest.items():
+        if m_re[1] > k_umok[1]:
+            m_res2.append(m_re[0])
+        else:
+            break
+    if mid in m_res2 and mid in u_res2:
         result.append(1)
         continue
+
     result.append(0)
 
 t = pd.DataFrame({
